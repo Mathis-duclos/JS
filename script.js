eacc20ee6a635f75ed678953caffcb20b3baccf5
Mathis Duclos <mathis.duclos@mmibordeaux.com>
2024-12-17 10:54:53 +0100

    aaaa

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-alert ("Coucou les p'tits loups")
+
+// faire ref à l'image
+let maRose = document.querySelector("img");
+
+// ajouter une action sur l'évent mouseover associé à l'image
+maRose.addEventListener("dblclick", action);
+
+//définir l'action
+function action() {
+    alert("Le riz d'Hélène");
+}
